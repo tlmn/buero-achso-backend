65349a0cd7e978121f565f4b62e5e00219f7cb9a
tlmn <mail@tlmn.io>
2022-03-08 21:22:32 +0100

    initial commit

---
+++ kirbycms/site/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'api' => [
+        'basicAuth' => true,
+        'allowInsecure' => true,
+    ],
+    'thumbs' => [
+        'srcsets' => [
+            'default' => [300, 800, 1024],
+        ],
+    ],
+];
